9a083a0b5e3ca2a4167622ec51465b49ff304235
Pascal Schedel <pilzinsel64@gmx.de>
2019-03-01 20:50:17 +0100

    Change Version to v0.7.3

--- SM64 ROM Manager/My Project/AssemblyInfo.vb
+++ SM64 ROM Manager/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports nUpdate.Core
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.7.2.0")>
+<Assembly: AssemblyVersion("0.7.3.0")>
