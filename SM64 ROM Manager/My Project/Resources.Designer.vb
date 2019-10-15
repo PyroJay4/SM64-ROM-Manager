1945bd4664dc3f08b459a5f2d651e21157f2989d
Pilzinsel64 <pilzinsel64@gmx.de>
2019-10-15 18:47:11 +0200

    change version to v1.0.5 b2

--- SM64 ROM Manager/My Project/Resources.Designer.vb
+++ SM64 ROM Manager/My Project/Resources.Designer.vb
@@ -61,7 +61,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Sucht eine lokalisierte Zeichenfolge, die 1 ähnelt.
+        '''  Sucht eine lokalisierte Zeichenfolge, die 2 ähnelt.
         '''</summary>
         Friend ReadOnly Property DevelopmentBuild() As String
             Get
