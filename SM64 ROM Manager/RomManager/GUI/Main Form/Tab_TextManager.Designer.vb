e6c1ff9511642848999148654a8895e801f4450a
Pilzinsel64 <pilzinsel64@gmx.de>
2019-11-07 07:41:18 +0100

    remove unused symbol properties

--- SM64 ROM Manager/RomManager/GUI/Main Form/Tab_TextManager.Designer.vb
+++ SM64 ROM Manager/RomManager/GUI/Main Form/Tab_TextManager.Designer.vb
@@ -285,9 +285,6 @@ Partial Class Tab_TextManager
         Me.ButtonItem_AddTextItem.Image = Global.SM64_ROM_Manager.My.Resources.MyIcons.icons8_plus_math_16px
         Me.ButtonItem_AddTextItem.ItemAlignment = DevComponents.DotNetBar.eItemAlignment.Far
         Me.ButtonItem_AddTextItem.Name = "ButtonItem_AddTextItem"
-        Me.ButtonItem_AddTextItem.SymbolColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(124, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.ButtonItem_AddTextItem.SymbolSet = DevComponents.DotNetBar.eSymbolSet.Material
-        Me.ButtonItem_AddTextItem.SymbolSize = 12.0!
         '
         'ButtonItem_RemoveTextItem
         '
@@ -295,9 +292,6 @@ Partial Class Tab_TextManager
         Me.ButtonItem_RemoveTextItem.Image = Global.SM64_ROM_Manager.My.Resources.MyIcons.icons8_delete_sign_16px
         Me.ButtonItem_RemoveTextItem.ItemAlignment = DevComponents.DotNetBar.eItemAlignment.Far
         Me.ButtonItem_RemoveTextItem.Name = "ButtonItem_RemoveTextItem"
-        Me.ButtonItem_RemoveTextItem.SymbolColor = System.Drawing.Color.FromArgb(CType(CType(150, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.ButtonItem_RemoveTextItem.SymbolSet = DevComponents.DotNetBar.eSymbolSet.Material
-        Me.ButtonItem_RemoveTextItem.SymbolSize = 12.0!
         '
         'Tab_TextManager
         '
