bca8f5e79dac6d20f98392facf7a6a41c76fb717
Pilzinsel64 <pilzinsel64@gmx.de>
2019-12-11 12:05:24 +0100

    change version to v1.3

--- SM64 ROM Manager/My Project/AssemblyInfo.vb
+++ SM64 ROM Manager/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports nUpdate.Core
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.0.0")>
+<Assembly: AssemblyVersion("1.3.0.0")>
