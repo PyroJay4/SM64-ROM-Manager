d1bf00a35115e99cebdef43352a48cde99d0fd4c
Pilzinsel64 <pilzinsel64@gmx.de>
2019-12-18 16:41:00 +0100

    change version to v1.4

--- SM64 ROM Manager/My Project/AssemblyInfo.vb
+++ SM64 ROM Manager/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.3.0.1")>
+<Assembly: AssemblyVersion("1.4.0.0")>
