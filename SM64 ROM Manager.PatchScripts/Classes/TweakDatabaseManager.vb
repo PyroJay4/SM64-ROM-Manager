54c427ae5d89ba2a45fcfd8619a6893fb984f0b8
Pilzinsel64 <pilzinsel64@gmx.de>
2020-02-05 08:11:23 +0100

    finish untested TweakDatabaseManager

--- SM64 ROM Manager.PatchScripts/Classes/TweakDatabaseManager.vb
+++ SM64 ROM Manager.PatchScripts/Classes/TweakDatabaseManager.vb
@@ -23,26 +23,108 @@ Public Class TweakDatabaseManager
 
     Public Async Function CheckForUpdates(localPath As String) As Task(Of IEnumerable(Of TweakDatabaseSyncFile))
         Dim client As WebDavClient = CreateClient(TweakDatabaseLoginTypes.ReadOnlyAll)
-        Dim list As New List(Of TweakDatabaseSyncFile)
+        Dim syncFiles As New List(Of TweakDatabaseSyncFile)
+        Dim checkOutFolder As Func(Of String, Task) = Nothing
+        Dim resTopFolder As WebDavResource = Nothing
 
-        Dim res As PropfindResponse = Await client.Propfind(Preferences.CategoryPaths(TweakDatabaseCategories.Reviewed))
-        Dim fileName As String = Path.GetFileName(res.Resources(1).Uri)
+        checkOutFolder =
+            Async Function(remotePath As String) As Task
+                Dim response As PropfindResponse = Await client.Propfind(remotePath)
 
-        '...
+                If resTopFolder Is Nothing AndAlso response.Resources.Any Then
+                    resTopFolder = response.Resources(0)
+                End If
 
-        client.Dispose()
-        Return list
-    End Function
+                If response.Resources.Count > 1 Then
+                    For i As Integer = 1 To response.Resources.Count - 1
+                        Dim res As WebDavResource = response.Resources(i)
+                        Dim isFolder As Boolean = False
 
-    Public Sub Update(syncFiles As IEnumerable(Of TweakDatabaseSyncFile))
-        Dim client As WebDavClient = CreateClient(TweakDatabaseLoginTypes.ReadOnlyAll)
+                        'Check for folder
+                        For Each prop As WebDavProperty In res.Properties
+                            If prop.Name = "{DAV:}getcontenttype" AndAlso prop.Value = "<d:collection xmlns:d=""DAV:"" />" Then
+                                isFolder = True
+                            End If
+                        Next
 
-        For Each syncFile As TweakDatabaseSyncFile In syncFiles
-            '...
+                        If isFolder Then
+                            Await checkOutFolder(res.Uri)
+                        Else
+                            Dim syncAction As TweakDatabaseSyncAction? = Nothing
+                            Dim localFile As String = String.Empty
+                            Dim remoteFile As String = String.Empty
+
+                            'Get remote file path
+                            remoteFile = res.Uri.Substring(Path.GetDirectoryName(resTopFolder.Uri).Length)
+
+                            'Get local file path
+                            localFile = Path.Combine(localPath, res.Uri.Substring(resTopFolder.Uri.Length))
+
+                            'Check action
+                            If File.Exists(localFile) Then
+                                If File.GetLastWriteTime(localFile) < res.LastModifiedDate Then
+                                    syncAction = TweakDatabaseSyncAction.UpdatedFile
+                                End If
+                            Else
+                                syncAction = TweakDatabaseSyncAction.NewFile
+                            End If
+
+                            'Add to list
+                            If syncAction IsNot Nothing Then
+                                syncFiles.Add(New TweakDatabaseSyncFile(syncAction, localFile, remoteFile))
+                            End If
+                        End If
+                    Next
+                End If
+            End Function
+
+        'Check for new & updated files
+        Await checkOutFolder(Preferences.CategoryPaths(TweakDatabaseCategories.Reviewed))
+
+        'Find all old files to remove
+        Dim allLocalFiles As String() = Directory.GetFiles(localPath, String.Empty, SearchOption.AllDirectories)
+        For Each lf As String In allLocalFiles
+            Dim isKnown As Boolean = False
+
+            For Each syncFile As TweakDatabaseSyncFile In syncFiles
+                If Not isKnown AndAlso syncFile.LocalFile = lf Then
+                    isKnown = True
+                End If
+            Next
+
+            If Not isKnown Then
+                syncFiles.Add(New TweakDatabaseSyncFile(TweakDatabaseSyncAction.RemovedFile, lf, String.Empty))
+            End If
         Next
 
         client.Dispose()
-    End Sub
+        Return syncFiles
+    End Function
+
+    Public Async Function Update(syncFiles As IEnumerable(Of TweakDatabaseSyncFile)) As Task
+        Dim client As WebDavClient = CreateClient(TweakDatabaseLoginTypes.ReadOnlyAll)
+
+        For Each syncFile As TweakDatabaseSyncFile In syncFiles
+            If syncFile.SyncAction = TweakDatabaseSyncAction.UpdatedFile OrElse syncFile.SyncAction = TweakDatabaseSyncAction.RemovedFile Then
+                File.Delete(syncFile.LocalFile)
+            End If
+
+            If syncFile.SyncAction = TweakDatabaseSyncAction.UpdatedFile OrElse syncFile.SyncAction = TweakDatabaseSyncAction.NewFile Then
+                Dim response As WebDavStreamResponse = Await client.GetProcessedFile(syncFile.RemoteFile)
+                Dim dirParent As New DirectoryInfo(Path.GetDirectoryName(syncFile.LocalFile))
+
+                If Not dirParent.Exists Then
+                    dirParent.Create()
+                End If
+
+                Dim fs As New FileStream(syncFile.LocalFile, FileMode.Create, FileAccess.Write)
+                response.Stream.CopyTo(fs)
+                fs.Close()
+            End If
+        Next
+
+        client.Dispose()
+    End Function
 
     Public Async Function Upload(fileName As String) As Task(Of Boolean)
         Dim client As WebDavClient = CreateClient(TweakDatabaseLoginTypes.UserUploads)
--- SM64 ROM Manager.PatchScripts/Classes/TweakDatabaseSyncFile.vb
+++ SM64 ROM Manager.PatchScripts/Classes/TweakDatabaseSyncFile.vb
@@ -4,7 +4,10 @@
     Public ReadOnly Property LocalFile As String
     Public ReadOnly Property RemoteFile As String
 
-    Friend Sub New()
+    Friend Sub New(syncAction As TweakDatabaseSyncAction, localFile As String, remoteFile As String)
+        Me.SyncAction = syncAction
+        Me.LocalFile = localFile
+        Me.RemoteFile = remoteFile
     End Sub
 
 End Class
